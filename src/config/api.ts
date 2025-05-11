4c409fa0e693f7dafabad32e200a1116fd26f809
devhasnain <hasnainalam1166@gmail.com>
2025-05-11 10:04:14 +0500

    #fix update api url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseApi = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_API_URL : "http://localhost:3000/api",
+  baseURL: process.env.NODE_ENV === "production" ? import.meta.env.VITE_API_URL : "http://localhost:3000/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
