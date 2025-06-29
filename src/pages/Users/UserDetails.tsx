a191d60140f5d27143cfefc422b43717e6010139
devhasnain <hasnainalam1166@gmail.com>
2025-06-29 09:03:19 +0500

    #fix added useEffect deps in update user form

--- src/pages/Users/UserDetails.tsx
+++ src/pages/Users/UserDetails.tsx
@@ -81,7 +81,7 @@ export default function UserDetails() {
         permissions: user?.permissions ?? initialValues.permissions,
       });
     }
-  }, [data]);
+  }, [data, id]);
 
   return (
     <>
