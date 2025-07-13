07ec5d80293a0cbf5f92083692235dbbaedfaebe
devhasnain <hasnainalam1166@gmail.com>
2025-07-13 15:57:33 +0500

    #fix updated logo

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 const baseApi = axios.create({
   baseURL: process.env.NODE_ENV === "production" ? import.meta.env.VITE_API_URL : "http://localhost:3000/api",
   timeout: 20000,
