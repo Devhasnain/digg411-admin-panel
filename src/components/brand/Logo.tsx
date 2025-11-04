d21c7e6457388f836300a73a4ef1f086fed6832d
devhasnain <hasnainalam1166@gmail.com>
2025-11-04 22:40:03 +0500

    Merge branch 'petro411' into digg411

--- src/components/brand/Logo.tsx
+++ src/components/brand/Logo.tsx
@@ -12,21 +12,20 @@ const Logo = ({ condition = true }: Props) => {
         <div className="flex flex-row items-end gap-2">
           <img
             className=""
-            src="/images/logo/logo.png"
-            alt="Logo"
-            width={40}
-            height={40}
-          />
-          <img
-            className=""
             src="/images/logo/logo-name.png"
             alt="Logo"
-            width={150}
+            width={200}
             height={40}
           />
         </div>
       ) : (
-        <img src="/images/logo/logo.png" alt="Logo" width={40} height={40} />
+        <img
+            className=""
+            src="/images/logo/logo-name.png"
+            alt="Logo"
+            width={100}
+            height={100}
+          />
       )}
     </>
   );
