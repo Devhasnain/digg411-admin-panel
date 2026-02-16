823695f2e30a8d061de8c409740405445a4b8e88
Devhasnain <hasnainalam1166@gmail.com>
2026-02-16 04:02:03 -0800

    #fix removed unused files

--- src/pages/MineralList/index.tsx
+++ src/pages/MineralList/index.tsx
@@ -274,8 +274,8 @@ const DeleteConfirmation = memo(({ id }: any) => {
   return (
     <>
       <TrashBinIcon className="h-4 w-4 cursor-pointer" onClick={openModal} />
-      <Modal onClose={closeModal} isOpen={isOpen} className="max-w-[700px]">
-        <div className="no-scrollbar relative w-full max-w-[700px] overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
+      <Modal onClose={closeModal} isOpen={isOpen} className="max-w-175">
+        <div className="no-scrollbar relative w-full max-w-175 overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
           <div className="px-2 pr-14">
             <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
               Confirm deletion
